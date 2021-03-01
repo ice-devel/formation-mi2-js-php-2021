e3fc634eae3ada1444b8790b2a64ba0a8608ba0d
fab <fab@mail.Fr>
2021-03-01 17:00:25 +0100

    correction exo

--- 2-php/8-exos.php
+++ 2-php/8-exos.php
@@ -52,23 +52,72 @@
     }
 
     /*
-     * Exo 2 : afficher la table de multiplication d'un nombre écrit en dur
+     * Exo 2 : afficher la table de multiplication d'un nombre codé en dur
      * 1 * 5 = 5
      * 2 * 5 = 10
      * ...
      * 9 * 5 = 45
      */
-    $nb = 5;
+
+    $nb = 9;
+    /*
+     * Bof bof :
+     */
+    // echo "1 * $nb = ".(1 * $nb)."<br>";
+    // echo "2 * $nb = ".(2 * $nb)."<br>";
+    // echo "3 * $nb = ".(3 * $nb)."<br>";
+
+    /*
+     * Bien bien :
+     */
+    for ($i=1; $i <=9; $i++) {
+        echo "$i * $nb = ".($i * $nb)."<br>";
+    }
+
     /*
      * Exo 3 :
-     * Déclarez un tableau, insérez 5 valeurs dedans puis écrire le script qui dit
-     * combien de fois se trouve la valeur "toto" dans ce tableau
+     * Déclarez un tableau, insérez 5 valeurs dedans puis écrire le script
+     * qui dit combien de fois se trouve la valeur "toto" dans ce tableau
      */
+    $firstnames = ["gege", "toto", "toto", "jeanjacques", "rené"];
+    // pourquoi pas :
+    $nbTotos = 0;
+    foreach ($firstnames as $fname) {
+        if ($fname == "toto") {
+            $nbTotos++;
+        }
+    }
+
+    // ou encore avec une fonction native
+    $firstnamesCount = array_count_values($firstnames);
+    echo "Nb de toto : ".$firstnamesCount['toto']."<br>";
     /*
      * Exo 4 :
      * Déclarez un tableau, initialisez le avec 6 valeurs entieres
      * Puis écrivez le script qui affiche la valeur la plus grande se trouvant dans  ce tableau
      */
+    $integers = [3, 45, 6, 34, 11];
+    $max = max($integers);
+
+    // ou alors :
+    $max = $integers[0];
+    foreach ($integers as $number) {
+        if ($number > $max) {
+            $max = $number;
+        }
+    }
+
+    // ou en optimisé (on compare pas le premier nb avec lui-même)
+    $max = $integers[0];
+    for ($i=1; $i<count($integers); $i++) {
+        if ($integers[$i] > $max) {
+            $max = $integers[$i];
+        }
+    }
+
+
+
+
 ?>
 
 
