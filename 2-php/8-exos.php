7a86613fc3c445338506ed73503e66b88dae23e9
fab <fab@mail.Fr>
2021-03-01 15:46:46 +0100

    correction exo 1

--- 2-php/8-exos.php
+++ 2-php/8-exos.php
@@ -6,6 +6,51 @@
     $currentYear = date('Y');
     $currentMonth = date('m');
     $currentDay = date('d');
+
+    $birthYear = 2000;
+    $birthMonth = 10;
+    $birthDay = 15;
+
+    // V1
+    // l'anniv est passé car le mois de naissance est passé
+    if ($currentMonth > $birthMonth) {
+        $age = $currentYear - $birthYear;
+    }
+
+    elseif ($birthMonth == $currentMonth) {
+        // l'anniv est passé car mois identitique mais jour de naissance passé
+        if ($birthDay <= $currentDay) {
+            $age = $currentYear - $birthYear;
+        }
+        // l'anniv n'est pas passé car mois identique mais jour de naissance pas encore passé
+        else {
+            $age = $currentYear - $birthYear - 1;
+        }
+    }
+    // l'anniv n'est pas encore passé car le mois actuel n'est pas encore arrivé au mois de naissance
+    else {
+        $age = $currentYear - $birthYear - 1;
+    }
+
+    // V2
+    if ($currentMonth > $birthMonth
+        || $birthMonth == $currentMonth && $birthDay <= $currentDay)
+    {
+        $age = $currentYear - $birthYear;
+    }
+    else {
+        $age = $currentYear - $birthYear - 1;
+    }
+
+    // V3
+    $age = $currentYear - $birthYear;
+    if ($currentMonth < $birthMonth
+        || $currentMonth == $birthMonth && $currentDay < $birthDay) {
+        //$age = $age - 1;
+        //$age -= 1;
+        $age--;
+    }
+
     /*
      * Exo 2 : afficher la table de multiplication d'un nombre écrit en dur
      * 1 * 5 = 5
