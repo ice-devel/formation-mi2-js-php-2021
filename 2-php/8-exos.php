a953f683cc5cd66db7602d4330ee7595a679890b
fab <fab@mail.Fr>
2021-03-01 14:44:04 +0100

    exo

---
+++ 2-php/8-exos.php
@@ -0,0 +1,37 @@
+<?php
+    /*
+     * Exo 1
+     * A partir d'une date de naissance (jour, mois, année) : Calculer l'âge
+     */
+    $currentYear = date('Y');
+    $currentMonth = date('m');
+    $currentDay = date('d');
+    /*
+     * Exo 2 : afficher la table de multiplication d'un nombre écrit en dur
+     * 1 * 5 = 5
+     * 2 * 5 = 10
+     * ...
+     * 9 * 5 = 45
+     */
+    $nb = 5;
+    /*
+     * Exo 3 :
+     * Déclarez un tableau, insérez 5 valeurs dedans puis écrire le script qui dit
+     * combien de fois se trouve la valeur "toto" dans ce tableau
+     */
+    /*
+     * Exo 4 :
+     * Déclarez un tableau, initialisez le avec 6 valeurs entieres
+     * Puis écrivez le script qui affiche la valeur la plus grande se trouvant dans  ce tableau
+     */
+?>
+
+
+
+
+
+
+
+
+
+
