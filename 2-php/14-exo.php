878b831c33da2e01288cc97c8517265a01f4a46d
fab <fab@mail.Fr>
2021-03-03 10:34:33 +0100

    correction exo debut

--- 2-php/14-exo.php
+++ 2-php/14-exo.php
@@ -17,17 +17,75 @@
      * - un pays dans une liste déroulante (select) - obligatoire -
      * la valeur doit forcément être une valeur de la liste
      *
-     * - Récupération donnée
+     * - Récupération données
      * - Vérification
      * - Le formulaire est bon :
      *  - afficher un message perso "Bienvenue #pseudo#"
      * - Le formulaire est pas bon :
      *  - vous affichez toutes les erreurs du form
      *  - remettre les valeurs saisies précédemment dans le formulaire
-     *
-     *
      */
+
+    $countries = ["France", "Norvège", "Japon", "Maroc", "Pays-Bas"];
+
+    // formulaire soumis ?
+    if (isset($_POST['btn-register'])) {
+        // récupération des valeurs
+        $pseudo = filter_input(INPUT_POST, 'pseudo');
+        $zipcode = filter_input(INPUT_POST, 'zipcode');
+        $country = filter_input(INPUT_POST, 'country');
+
+        // vérification
+        $errors = [];
+        if (mb_strlen($pseudo) < 5) {
+            $errors['pseudo'] = "Le pseudo est incorrect.";
+        }
+
+        if (!preg_match("/^(2[0-9AB]|[0-9]{2})[0-9]{3}$/", $zipcode)) {
+            $errors['zipcode'] = "Le code postal est incorrect.";
+        }
+
+        if (!in_array($country, $countries)) {
+            $errors['country'] = "Le pays est incorrect.";
+        }
+
+        // envoi en bdd..
+        if (empty($errors)) {
+            $success = true;
+            // redirection, mail, etc.
+        }
+        else {
+            $message = "Erreurs";
+        }
+    }
 ?>
+
+    <?php
+        if (isset($message)) {
+            echo "<p>ERREURS</p>";
+
+            foreach ($errors as $error) {
+                echo "<p>$error</p>";
+            }
+        }
+        elseif (isset($success)) {
+            echo "<p>Bienvenue $pseudo</p>";
+        }
+    ?>
+    <form action="" method="post">
+        <input type="text" placeholder="Pseudo" name="pseudo" />
+        <input type="text" placeholder="Code postal" name="zipcode" />
+        <select name="country">
+            <option></option>
+            <?php
+                foreach ($countries as $country) {
+                    echo "<option>$country</option>";
+                }
+            ?>
+        </select>
+        <input type="submit" name="btn-register" />
+    </form>
+
 </body>
 </html>
 
