35b3f0e62316547a0c9096dda8deabbc15dbdb72
fab <fab@mail.Fr>
2021-03-16 11:03:45 +0100

    exo poo mvc

---
+++ 2-php/6-crud-poo-mvc/exercice.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Exercice CRUD POO MVC
+ * Coder l'affichage et la création de jeux vidéos dans un magasin
+ * Pour le jeux : deux infos : le nom et le prix
+ * 1 - Base de données : création de la table
+ * 2 - Création du projet sur le système de fichier dans le dossier htdocs / www
+ * avec vite zef l'archi mvc (public src controller etc.)
+ * 3 - redirection d'un faux domaine dans le fichier host / vhost correspondant dans apache
+ * 4 - créer le controller frontal et afficher du texte pour vérifier que le vhost est ok
+ * 5 - composer : mettre en place l'autoload avec les commandes de composer (installation / config)
+ * 6 - créer une page de test en MVC
+ * 7 - faites le crud (affichage / création) jeux videos en poo mvc
+ *
+ */
