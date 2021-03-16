8d57a5c3756dd36ecb0c8119393e15c50243925c
fab <fab@mail.Fr>
2021-03-16 16:57:12 +0100

    reference

---
+++ 2-php/5-poo/8-reference.php
@@ -0,0 +1,88 @@
+<?php
+require 'crud/User.php';
+require 'crud/UserManager.php';
+
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+
+$pdo = new PDO("mysql:host=".DB_HOST.";dbname=formation_202103;charset=utf8", DB_USER, DB_PASS);
+
+$userManager = new UserManager($pdo);
+$users = $userManager->getAll();
+
+if (isset($_POST['insert-user'])) {
+    $user = $userManager->handleRequest();
+    $errors = $userManager->validate($user);
+
+    if (empty($errors)) {
+        if ($userManager->insert($user)) {
+            header('Location: 7-crud.php');
+        }
+    }
+}
+
+// suppression
+if (isset($_GET['action']) && $_GET['action'] == "delete" && isset($_GET['id'])) {
+    // supprimer l'utilisateur dont l'id a été passé dans l'url
+    $id = filter_input(INPUT_GET, 'id');
+
+    if ($id != null) {
+        $id = intval($id);
+        $user = $userManager->getOne($id);
+
+        if ($user != null) {
+            $userManager->delete($user);
+        }
+        else {
+            $message = "L'utilisateur n'existe pas/plus.";
+        }
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD POO</title>
+</head>
+<body>
+    <h1>Afficher/Supprimer les users</h1>
+
+    <table>
+        <?php foreach ($users as $user): ?>
+        <tr>
+            <td><?= $user->getId() ?></td>
+            <td><?= $user->getUsername() ?></td>
+            <td><?= $user->getPassword() ?></td>
+            <td>
+                <a href="?action=update&id=<?= $user->getId() ?>">Modifier</a>
+                <a href="?action=delete&id=<?= $user->getId() ?>">Supprimer</a>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </table>
+
+    <h1>Créer un user</h1>
+    <?php
+    ?>
+    <form method="post">
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="submit" name="insert-user"/>
+    </form>
+
+    <h1>Modifier un user</h1>
+
+    <form method="post">
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="submit" name="update-user"/>
+    </form>
+</body>
+</html>
